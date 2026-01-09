f5d2cc2e63428053e39eac52b3c4659e7e7eb8f1
technalazi <azinuruddin@gmail.com>
2026-01-10 02:11:09 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
